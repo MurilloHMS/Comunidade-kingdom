92030879ae4c185a6909d9d765030f2d3769304d
MurilloHMS <murillohenriquemelo@outlook.com>
2023-04-14 15:47:05 -0300

    bugs corrigidos

--- kingdom/admin.py
+++ kingdom/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import NovoTestamento
 
-# Register your models here.
+admin.site.register(NovoTestamento)
--- kingdom/models.py
+++ kingdom/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class NovoTestamento(models.Model):
+    livro = models.CharField(max_length=100)
+    abreviacao = models.CharField(max_length=10)
